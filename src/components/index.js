42211eb9ba0225148f001e8d47845752fca9da1f
shaldi1710 <faishalhadi.hf@gmail.com>
2023-03-14 22:47:58 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import ListCategories from "./ListCategories"
+import Hasil from "./Hasil"
+import NavbarComponents from "./NavbarComponents"
+import Menus from "./Menus"
+
+
+
+export {ListCategories, Hasil, NavbarComponents, Menus}
